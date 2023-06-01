a7d7f336236a7edf18e4d05c96742a28dc763a5a
MuhammedNihalcp <nihalriju915@gmail.com>
2023-06-01 20:13:38 +0530

    add app icon image

--- lib/screens/splash_page/controller/splash_controller.dart
+++ lib/screens/splash_page/controller/splash_controller.dart
@@ -1,5 +1,18 @@
-import 'package:get/get.dart';
+import 'dart:async';
 
-class SplashController extends GetxController{
-  
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+import 'package:news_app/screens/auth/sign_in/view/sign_in_view.dart';
+import 'package:news_app/screens/home/view/home_view.dart';
+
+class SplashController extends GetxController {
+  void splashTimer() {
+    Timer(const Duration(seconds: 3), () async {
+      if (FirebaseAuth.instance.currentUser != null) {
+        Get.offAll(() => ScreenHome());
+      } else {
+        Get.offAll(() => ScreenSignIn()); 
+      }
+    });
+  }
 }
--- lib/screens/splash_page/view/splash_view.dart
+++ lib/screens/splash_page/view/splash_view.dart
@@ -1,16 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:news_app/core/colors.dart';
+import 'package:news_app/screens/splash_page/controller/splash_controller.dart';
 
 class ScreenSplash extends StatelessWidget {
-  const ScreenSplash({super.key});
+  ScreenSplash({super.key});
+
+  SplashController splashController = Get.put(SplashController());
+  double height = Get.height;
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: backgroundColor,
+    WidgetsFlutterBinding.ensureInitialized().addPostFrameCallback((timeStamp) {
+      splashController.splashTimer();
+    });
+    return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Text('Splash Screen'),
+        child: Container(
+          width: double.infinity,
+          height: height,
+          decoration: const BoxDecoration(
+            color: backgroundColor,
+            // gradient: LinearGradient(
+            //   begin: Alignment.topCenter,
+            //   end: Alignment.bottomCenter,
+            //   colors: [backgroundColor, buttoncolor],
+            // ),
+          ),
+          child: const Center(
+            child: Image(
+              image: AssetImage('assets/images/news_app_Icon.png'),
+            ),
+          ),
         ),
       ),
     );
