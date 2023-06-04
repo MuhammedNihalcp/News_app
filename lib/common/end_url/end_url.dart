151790da25094fa94a17e899a2fbd988ac8f1e59
MuhammedNihalcp <nihalriju915@gmail.com>
2023-06-04 21:01:24 +0530

    add endurls

--- lib/common/end_url/end_url.dart
+++ lib/common/end_url/end_url.dart
@@ -3,4 +3,12 @@ class ApiEndUrl {
   String science = 'science';
   String business = 'business';
   String sports = 'sports';
+  String world = 'world';
+  String politics = 'politics';
+  String technology = 'technology';
+  String startup = 'startup';
+  String entertainment = 'entertainment';
+  String hatke = 'hatke';
+  String automobile = 'automobile';
+  String miscellaneous = 'miscellaneous';
 }
